a97a88966b4ffc07b754ed8046dc0d30550245df
johndv023 <158854739+johnChk0@users.noreply.github.com>
2024-04-06 01:06:19 -0300

    Aula 13, 14 (JavaScript CFBCursos)
    Curso de JavaScript (CFBCursos)
    Aula 13
    Aula 14
    + anotações

---
+++ Atividades/Aula 13/aula13.js
@@ -0,0 +1,21 @@
+
+//if(5>4){
+//    console.log(`Bloco de codigo 1`);
+//}else{
+//    console.log(`Bloco de codigo 2`)
+//}
+
+/* // // // // // // // // // // // // // // // // // /*/
+
+//let num= 10
+//if(num<=3){
+//    console.log('num e maior que 3')
+//}else if(num>6){
+//    console.log("num e maior que 6")
+//}else if(num>7 && num!=100){
+//    console.log("Num e maior que 7")
+//}else{
+//    console.log('nukm e maior que todos os numeros')
+//}
+
+
---
+++ Atividades/Aula 14/aula14.js
@@ -0,0 +1,16 @@
+let colocacao = 1
+switch(colocacao){
+    case 1:
+        console.log("Primeiro Lugar")
+        break
+    case 2:
+        console.log("Segundo Lugar")
+        break
+    case 3: case 4: case 5:
+        console.log("Terceiro Lugar")
+        break
+    default:
+        console.log("Não subiu ao pódio")
+        break
+
+}   
